1241cf8f2ae7d1cf138daa88225a8633e646606d
uk921 <hyeonuk0306@gmail.com>
2026-09-23 02:49:25 +0000

    prob1

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
-int main(){
-	printf("Hello World");
+#include "copy.h"
+
+char line[MAXLINE];
+char longest[MAXLINE];
+
+main(){
+	int len;
+	int max;
+	max = 0;
+
+	while(gets(line) != NULL) {
+		len = strlen(line);
+
+		if(len > max) {
+			max = len;
+			copy(line, longest);
+			}	
+		}
+
+	if(max > 0)
+		printf("%s \n", longest);
+
 	return 0;
 }
